104f27dc933f2b1b6ef16d530b31c79556fdce9a
alioishi <29942094+alioishi@users.noreply.github.com>
2022-06-22 17:58:12 -0700

    Refactor addGridElements for readability

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 function createGrid(gridSize){
     const grid = document.createElement('div');
-    initializeGrid(grid, gridSize);
+    initGrid(grid, gridSize);
     addGridElements(grid, gridSize)
     document.body.appendChild(grid);
 }
 
 // set size of grid elements to 25px for now until i figure out how to make it scale
-function initializeGrid(grid, gridSize){
+function initGrid(grid, gridSize){
     grid.setAttribute('id', 'grid');
     grid.style.display = 'grid';
     grid.style.gridTemplateRows = `repeat(${gridSize}, 25px)`;
     grid.style.gridTemplateColumns = `repeat(${gridSize}, 25px)`;
 }
 
-// id's are set to row#-col# for selection
 function addGridElements(grid, gridSize){
-    for(i = 1; i <= gridSize; i++){
-        for(j = 1; j <= gridSize; j++){
-            const div = document.createElement('div');
-            div.setAttribute('id', `row${i}-col${j}`);
-            div.style.gridRow = `${i} / ${i+1}`;
-            div.style.gridColumn = `${j} / ${j+1}`;
-            grid.appendChild(div);
+    for(row = 1; row <= gridSize; row++){
+        for(col = 1; col <= gridSize; col++){
+            const gridElement = document.createElement('div');
+            initGridElement(gridElement, row, col);
+            grid.appendChild(gridElement);
         }
     }
+}
+
+// id's are set to row#-col# for selection
+function initGridElement(gridElement, row, col){
+    gridElement.setAttribute('id', `row${row}-col${col}`);
+    gridElement.style.gridRow = `${row} / ${row+1}`;
+    gridElement.style.gridColumn = `${col} / ${col+1}`;
 }
 
 createGrid(16);
